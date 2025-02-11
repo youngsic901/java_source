d2f39fafdf23c5579100576c7d190a009278ef45
youngsic <youngsic901@gmail.com>
2025-02-11 16:57:25 +0900

    main 브랜치 내용 추가

--- java_basic/src/pack/HelloTest.java
+++ java_basic/src/pack/HelloTest.java
@@ -5,5 +5,6 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("프로그램 작성");
 		System.out.println("hotfix 브랜치 내용 변경");
+		System.out.println("main 브랜치 내용 추가");
 	}
 }
